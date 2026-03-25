bf23e0017450d651cdfd2e666347ae214e7a0d2e
Tano <github@tano.asia>
2026-03-25 15:17:32 +0800

    refactor: move content to a separate repository

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,13 +5,12 @@ import typography from '@tailwindcss/typography'
 export default <Partial<Config>>{
     theme: {
         extend: {
-            // 你可以在这里自定义你的主题颜色
             colors: {
                 primary: '#f97316', // 橙色主题
             }
         }
     },
     plugins: [
-        typography, // 核心：开启 Markdown 排版支持
+        typography,
     ],
 }
