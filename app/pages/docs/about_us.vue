6ce03cc8593c5b92b9d8823f9eee522a187f386c
Tano <github@tano.asia>
2026-03-30 23:44:05 +0800

    feat: 修改同好会介绍页面

--- app/pages/docs/about_us.vue
+++ app/pages/docs/about_us.vue
@@ -72,7 +72,7 @@
           </div>
           <p>4) 「同好会」致力于结合过往的管理与实验经验、全球 ACGN 文化社区的历史与现状，以及现有的相关专业理论分析等内容，形成一系列研究成果，并将其与「同好会」的实际情况相结合，最终应用于组织内部的各个方面，例如「联协体系」中的组织分类办法，以及「栖心回响」在群员管理中的具体实践等。我们由衷希望这些成果能够为其他组织或相关领域的研究者提供案例分析与研究参考，进而推动整体环境的不断向好。</p>
           <p>5) 「同好会」的前身与诞生基础是「WTW団」与「パラレルクロス社」两大企划。</p>
-          <div class="pl-4 border-l border-white/10 text-gray-400 space-y-4 text-sm">
+          <div class="pl-4 border-l border-white/10 text-gray-400 space-y-4 ">
             <p>目前，「WTW団」已完全关闭成员加入渠道，其遗产仅作为「同好会」人员职务与研究的参考资料。该企划起源于2018年至2019年，现任团长为星天耀Star，副团长为秦。在「WTW団」运作期间，现任团长开展了一系列实验与试错，为现「同好会」的成员管理与活动开展积累了丰富经验，并确立了当前「同好会」的宗旨与目标。「WTW団」企划于2022年进入完全冷藏期，最终在2024年因现任团长个人情况而解散群聊。</p>
             <p>「パラレルクロス社」企划现已转为「同好会」官方对外项目的唯一窗口。该企划起源于2021年，主要负责人为秦。在「パラレルクロス社」活动期间，初步形成了「联协体系」的雏形，并为现行体系提供了宝贵的参考数据。该企划于2022年因主要负责人个人情况而临时解散，目前转为与「同好会」相对独立的“窗口”，负责「同好会」官方对外项目的公开与可视化工作。</p>
           </div>
@@ -97,7 +97,12 @@
               <p>「让我们从这里出发——不念过往，不留遗憾，尽情享受，尽力前行。」</p>
               <p>「最后，再次在这里相遇，与大家道一声：欢迎回来。」</p>
               <p>「对我来说，人生最浪漫的时刻，莫过于此」</p>
-              <p class="text-right text-xs font-bold text-blue-400">WTW団副团长 跨世界同好会会长 秦</p>
+            </div>
+            <div class="flex flex-col items-end pt-6">
+              <div class="w-16 h-[1px] bg-white/10 my-2"></div>
+              <p class="text-xs not-italic tracking-normal leading-normal text-gray-500">
+                ————  WTW団副团长 跨世界同好会会长 秦
+              </p>
             </div>
 
             <div class="space-y-2 border-t border-white/5 pt-6">
@@ -116,7 +121,12 @@
               <p>「而现在同好会成立将近一年了，每一步都离不开各位的支持。」</p>
               <p>「正是因为有你们在，我才能把这一切变成现实。」</p>
               <p>「由衷感谢跨世界同好会的各位，还有读到这里的你。」</p>
-              <p class="text-right text-xs font-bold text-indigo-400">WTW団现任团长 跨世界同好会主干成员 星天耀Star</p>
+            </div>
+            <div class="flex flex-col items-end pt-6">
+              <div class="w-16 h-[1px] bg-white/10 my-2"></div>
+              <p class="text-xs not-italic tracking-normal leading-normal text-gray-500">
+                ————  WTW団现任团长 跨世界同好会主干成员 星天耀Star
+              </p>
             </div>
           </div>
 
